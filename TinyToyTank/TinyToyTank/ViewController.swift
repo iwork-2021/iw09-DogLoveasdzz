8627f5b5d2fd20cb1a4667fca0163de5d2177b4e
nju <nju@njudeiMac.local>
2022-01-05 18:44:35 +0800

    try code

--- TinyToyTank/TinyToyTank/ViewController.swift
+++ TinyToyTank/TinyToyTank/ViewController.swift
@@ -12,11 +12,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var arView: ARView!
     var tankAnchor: TinyToyTank._TinyToyTank?
+    var isActionPlaying: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tankAnchor = try! TinyToyTank.load_TinyToyTank()
+        
+        tankAnchor!.turret?.setParent(tankAnchor!.tank, preservingWorldTransform: true)
+        
+        tankAnchor?.actions.actionComplete.onAction = { _ in self.isActionPlaying = false}
         
         arView.scene.anchors.append(tankAnchor!)
         // Load the "Box" scene from the "Experience" Reality File
@@ -26,16 +31,34 @@ class ViewController: UIViewController {
         //arView.scene.anchors.append(boxAnchor)
     }
     @IBAction func turretLeft(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.turretLeft.post()
     }
     @IBAction func cannonFire(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.cannonFire.post()
     }
     @IBAction func turretRight(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.turretRight.post()
     }
     @IBAction func tankLeft(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankLeft.post()
     }
     @IBAction func tankForward(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankForward.post()
     }
     @IBAction func tankRight(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankRight.post()
     }
     
 }
